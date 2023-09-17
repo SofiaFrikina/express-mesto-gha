c757a8890fa2b579d172704f45b4f0ba95ea2119
SofiaFrikina <sofi.frikina@yandex.ru>
2023-09-17 21:21:13 +0300

    добавление в роутеры обработки ошибки

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const userRouter = require('./users')
 const cardRouter = require('./cards')
+const ERROR_NOT_FOUND = require('../errors/errors')
 
 router.use('/users', userRouter)
 router.use('/cards', cardRouter)
+router.use('*', (req, res) => {
+  res.status(ERROR_NOT_FOUND).send({ message: 'Переданы некорректные данные' })
+})
 
 module.exports = router;
