8775a5e97d5abafbae08933e0da4ffa05dc1bf4b
SofiaFrikina <sofi.frikina@yandex.ru>
2023-10-03 17:00:07 +0300

    добавила в полях name, about, avatar значения по умолчанию

--- models/user.js
+++ models/user.js
@@ -5,16 +5,19 @@ const AthorizedError = require('../utils/errors/AthorizedError');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
+    default: 'Жак-Ив Кусто',
     minlength: [2, 'Минимальная длина поля "name" - 2'],
     maxlength: [30, 'Максимальная длина поля "name" - 30'],
   },
   about: {
     type: String,
+    default: 'Исследователь',
     minlength: [2, 'Минимальная длина поля "name" - 2'],
     maxlength: [30, 'Максимальная длина поля "name" - 30'],
   },
   avatar: {
     type: String,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
     type: String,
